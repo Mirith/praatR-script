7c26ae48a112dd40f656a52094853d36fcc15be6
Lauren Shin <laurenshin15@gmail.com>
2018-02-14 18:15:55 -0800

    initial commit

---
+++ script.R
@@ -0,0 +1,23 @@
+# first script attempt
+# goals: 
+#
+# measure F1, F2, F3 11 (?) times equidistant
+# Mean F1, F2, F3, amplitude, F0
+# length
+# 1st quartile amplitude, F0
+# 3rd quartile amplitude, F0
+# median amplitude, F0
+
+# multiple scripts? one big script?
+# multiple seems easier and neater, more modular
+# have open in praat or select from directory? multiple vowels? 
+# dictionary of lists for sounds in context
+# ie CVC : list of relevant numbers in certain order
+# or dictionary of dictionary, with value being labeled numbers
+# so big for loop should construct a dictionary, add it to other dictionary?
+
+# multiple little pieces 
+# measuring total length of ___
+# computing average is built in to R
+# measure median of ___
+# quartile amplitude can call that one... 
