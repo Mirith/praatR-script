ecab8e512ac53183180a9d6407cad5f846171beb
Lauren Shin <laurenshin15@gmail.com>
2018-03-10 17:51:55 -0800

    added mean_f0, mean_amp, and f_sample

--- script.R
+++ script.R
@@ -8,38 +8,19 @@
 # median amplitude, F0
 # peak F0 and amplitude
 
-# deal with formatting outputs later, just get pieces in place
-# eventually want to make a big dataframe for everything to work with
-
-# looking in praat at buttons/menus gives explanation of each set of parameters
-
-# to do -- vowels v consonants analysis?  
-# consonants and pitch...
-
 rm(list = ls())
 
 library(PraatR)
 
-# text grid and sound path should be automatically grabbed later
-# following are hard-coded for testing purposes
-# path for text grid file
-path = "C:/praatR/Bitur/grids/abua-DM-1.TextGrid"
-# path for sound file
-sound_path = "C:/praatR/Bitur/audio/abua-DM-1.wav"
-# path for outputs of converted files
-# will be overwritten many times as script runs
-inten_path = "C:/praatR/Bitur/audio/inten.Matrix"
-pitch1_path = "C:/praatR/Bitur/audio/untitled.Pitch"
-
 # when passed a path of a text grid
 # returns start and stop times with interval label as a list within a list
-get_start_end <- function(text_grid)
+get_start_end <- function(grid_loc)
 {
     # stores the total number of intervals in the text grid
     interval_numbers = as.numeric(
         praat("Get number of intervals...", # praat command
               list(1), # 1 = the number of the tier to look at
-              input = text_grid, # path of text grid
+              input = grid_loc, # path of text grid
               simplify = TRUE)) # return only number (still a string)
     # empty, to be filled and returned later
     return_list <- vector("list")
@@ -50,7 +31,7 @@ get_start_end <- function(text_grid)
         # gets the label of the interval
         interval_label = praat("Get label of interval...", # praat command
                                list(1, interval), # tier and interval to look at
-                               input = text_grid)
+                               input = grid_loc)
         # if it has a label
         if(interval_label != "")
         {
@@ -58,13 +39,13 @@ get_start_end <- function(text_grid)
             # similar to how Get label of interval... works
             interval_start = as.numeric(praat("Get start point...", 
                                               list(1, interval), 
-                                              input = text_grid, 
+                                              input = grid_loc, 
                                               simplify = TRUE))
             # and the end point
             # similar to how Get label of interval... works
             interval_end = as.numeric(praat("Get end point...", 
                                             list(1, interval), 
-                                            input = text_grid, 
+                                            input = grid_loc, 
                                             simplify = TRUE))
             
             # adds the interval label and start/end points to the end of the list
@@ -117,8 +98,6 @@ formant_means <- function(fmean_interval, formant_loc)
     
     return (adding)
 }
-
-# formant_means(c(.0878, .21314), "C:/praatR/Bitur/temp/formant.Matrix")
 
 # quartile and median calculation
 # amplitude
@@ -279,35 +258,96 @@ quart_med_amp <- function(amp_interval, intensity_loc)
 
 # returns max F0 value of interval passed 
 # f0_time is a list of the start and end point of the interval to look at
-max_F0 <- function(f0_time, f0_path)
+max_F0 <- function(f0_time, f0_loc)
 {
     start = f0_time[1]
     end = f0_time[2]
     
     val = as.numeric(praat("Get maximum...",
                      list(start, end, "Hertz", "Parabolic"),
-                     input = f0_path,
+                     input = f0_loc,
                      simplify = TRUE))
     return (val)
 }
 
-# max_F0(c(.0878, .21314), pitch_path)
-
 # max amplitude value finder of interval passed 
 # amp_time is a list of the start and end point of the interval to look at 
-max_amp <- function(amp_time, intensity_path)
+max_amp <- function(amp_time, intensity_loc)
 {
     start = amp_time[1]
     end = amp_time[2]
     
     val = as.numeric(praat("Get maximum...",
                            list(start, end, "Parabolic"),
-                           input = intensity_path,
+                           input = intensity_loc,
                            simplify = TRUE))
     return (val)
 }
 
-# max_amp(c(.0878, .21314), inten_path)
+# f0_interval is a list of the start and end point of the interval to look at
+mean_F0 <- function(f0_interval, f0_loc)
+{
+    start = f0_interval[1]
+    end = f0_interval[2]
+    
+    mean = as.numeric(praat("Get mean...", 
+                            list(start, end, "Hertz"), 
+                            input = f0_loc, 
+                            simplify = TRUE))
+    return (mean)
+}
+
+# amp_interval is a list of the start and end point of the interval to look at
+mean_amp <- function(amp_interval, amp_loc)
+{
+    start = amp_interval[1]
+    end = amp_interval[2]
+    
+    mean = as.numeric(praat("Get mean...", 
+                            list(start, end, "energy"), 
+                            input = amp_loc, 
+                            simplify = TRUE))
+    return (mean)
+}
+
+# f_interval is a list of the start and end point of the interval to look at
+f_sample <- function(f_interval, num_samples, formant_loc)
+{
+    split = interval_split(c(f_interval[1], f_interval[2]), num_samples)
+    return_vals = vector("list")
+    
+    f1 = c()
+    f2 = c()
+    f3 = c()
+    
+    for (point in split)
+    {
+        f1_val = as.numeric(praat("Get value at time...", 
+                                 list(1, point, "Hertz", "Linear"),
+                                 input = formant_loc,
+                                 simplify = TRUE))
+        
+        f2_val = as.numeric(praat("Get value at time...", 
+                                  list(2, point, "Hertz", "Linear"),
+                                  input = formant_loc,
+                                  simplify = TRUE))
+        
+        f3_val = as.numeric(praat("Get value at time...", 
+                                  list(3, point, "Hertz", "Linear"),
+                                  input = formant_loc,
+                                  simplify = TRUE))
+        
+        f1 = append(f1, f1_val)
+        f2 = append(f2, f2_val)
+        f3 = append(f3, f3_val)
+    }
+    
+    return_vals[[1]] = f1
+    return_vals[[2]] = f2
+    return_vals[[3]] = f3
+    
+    return (return_vals)
+}
 
 # get_start_end(path)
 # returns a list of points in time split into number_splits points
@@ -420,21 +460,41 @@ for (file in grid_list[1:3])
     {
         start = interval[1]
         end = interval[2]
-        temp_interval = c(start, end)
-
-        label = names(start)
-        f_means = formant_means(temp_interval, formant_path)
-        qma = quart_med_amp(temp_interval, intensity_path)
-        length = end - start 
-        mf0 = max_F0(temp_interval, pitch_path)
-        ma = max_amp(temp_interval, intensity_path)
         
-        nums_return = c(length, mf0, ma)
-        names(nums_return) = c("length", "max f0", "max amp")
+        label = names(start)
+        f_means = formant_means(interval, formant_path)
+        qma = quart_med_amp(interval, intensity_path)
+        length = end - start 
+        f0_max = max_F0(interval, pitch_path)
+        amp_max = max_amp(interval, intensity_path)
+        f0_mean = mean_F0(interval, pitch_path)
+        amp_mean = mean_amp(interval, intensity_path)
+        f_pts = f_sample(interval, 11, formant_path)
+        
+        # start = interval[1]
+        # end = interval[2]
+        # temp_interval = c(start, end)
+        # 
+        # label = names(start)
+        # f_means = formant_means(temp_interval, formant_path)
+        # qma = quart_med_amp(temp_interval, intensity_path)
+        # length = end - start 
+        # f0_max = max_F0(temp_interval, pitch_path)
+        # amp_max = max_amp(temp_interval, intensity_path)
+        # f0_mean = mean_F0(temp_interval, pitch_path)
+        # amp_mean = mean_amp(temp_interval, intensity_path)
+        
+        nums_return = c(length, f0_max, amp_max, f0_mean, amp_mean)
+        names(nums_return) = c("length (s)", 
+                               "max f0 (Hz)", 
+                               "max amp (dB)", 
+                               "mean f0 (Hz)", 
+                               "mean amp (dB)")
         
         print(f_means)
         print(qma)
         print(nums_return)
+        print(f_pts)
     }
 }
 
